1043739337e5d3bdb8a39a4e7effd12d50206ed7
sunman7 <473910527@qq.com>
2021-02-22 19:31:32 +0800

    todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+        <div class="container">
+            <h1>记录你接下来要做的事吧~</h1>
+            <Add :tid="todos.length" @add-todo="addTodo"></Add>
+            <Filter :selected="filter" @change-filter="filter = $event"></Filter>
+            <List :todos="filteredTodos"></List>
+        </div>
+    </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import List from "./components/List";
+    import Add from "./components/Add";
+    import Filter from "./components/Filter";
+    import {computed, ref} from "vue";
+
+    export default {
+        name: "App",
+        components: {
+            List, Add, Filter
+        },
+        setup() {
+            const filter = ref("all");
+            const filteredTodos = computed(() => {
+                switch (filter.value) {
+                    case "done":
+                        return todos.value.filter((todo) => todo.completed);
+                    case "todo":
+                        return todos.value.filter((todo) => !todo.completed);
+                    default:
+                        return todos.value;
+                }
+            });
+            const todos = ref([]);
+            const addTodo = (todo) => todos.value.push(todo);
+            return {
+                todos, addTodo, filter, filteredTodos
+            };
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: Helvetica, "PingFang SC", "Microsoft YaHei UI", sans-serif;
+    }
+
+    main {
+        width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: rgb(203, 210, 240);
+    }
+
+    .container {
+        width: 80%;
+        max-width: 400px;
+        box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+        border-radius: 24px;
+        padding: 48px 28px;
+        background: rgba(245, 246, 252);
+    }
+
+    h1 {
+        margin: 24px 0;
+        font-size: 28px;
+        color: #414873;
+        text-align: center;
+    }
+
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="todo-list">
+        <ListItem v-for="todo in todos" :key="todo.id" :todo-item="todo"
+                  @change="todo.completed = $event.target.checked"></ListItem>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import ListItem from "./ListItem";
+
+    export default {
+        components: {
+            ListItem
+        },
+        props: ["todos"]
+
+    }
+    ;
+</script>
+
+<style scoped>
+    .todo-list {
+        display: grid;
+        row-gap: 14px;
+    }
+
+</style>
